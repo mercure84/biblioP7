35dc2403ec4d96112841fd00beb0f0683db7fa14
mercure84 <julien.marcesse@gmail.com>
2019-08-10 01:02:52 +0200

    implémentation du scénario de connexion

--- src/main/java/com/biblioP7/webController/SignInController.java
+++ src/main/java/com/biblioP7/webController/SignInController.java
@@ -5,6 +5,7 @@ import com.biblioP7.beans.LoginForm;
 import com.biblioP7.feignClient.MembreServiceClient;
 
 import com.biblioP7.security.JwtRequest;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,9 +13,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 
 @Controller
+@SessionAttributes("reponseAuth")
 public class SignInController {
 
 
@@ -23,38 +26,49 @@ public class SignInController {
 
 
     @GetMapping("/client/signIn")
-    public String signInPage (Model model){
+    public String signInPage(Model model, String connexion) {
         model.addAttribute("login", new LoginForm());
+        try{
+            if(connexion.equals("erreur")){
+                model.addAttribute("connexion", "nOK");
+            }
+        }
+        catch(Exception e)
+
+    {
+
+    }
+
         return "signIn";
     }
 
 
     @PostMapping("/client/signIn")
-    public String seConnecter(@ModelAttribute LoginForm login, Model model){
+    public String seConnecter(@ModelAttribute LoginForm login, Model model) {
 
         try {
-
             JwtRequest jwtRequest = new JwtRequest();
             jwtRequest.setPassword(login.getPassword());
             jwtRequest.setUsername(login.getEmail());
+
             ResponseEntity<?> reponseAuth = membreServiceClient.login(jwtRequest);
-            model.addAttribute("token" , reponseAuth );
-            System.out.println(reponseAuth);
-            return "index";
+            if (reponseAuth.getStatusCode().value() == 200) {
+                String token = reponseAuth.getBody().toString();
+                model.addAttribute("token", token);
+                model.addAttribute("connexion", "OK");
+                return "index";
 
-        } catch (Exception e){
+            } else {
+                return "redirect:/client/signIn?connexion=erreur";
+            }
 
-            return "error";
+        } catch (Exception e) {
+            return "redirect:/client/signIn?connexion=erreur";
+
         }
 
 
-
-
-
-
-
     }
-
 
 
 }
